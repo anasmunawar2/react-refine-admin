4e47e786d4b70c0485741a03354080f98fb2ebc9
anasmunawar2 <anasmunawar2@gmail.com>
2024-05-01 02:51:05 +0500

    home-page working

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,18 +1,29 @@
 import { CalendarOutlined } from "@ant-design/icons";
 import { Badge, Card, List } from "antd";
-import { useState } from "react";
 import { Text } from "../text";
 import UpcomingEventsSkeleton from "../skeleton/upcoming-events";
 import { getDate } from "@/utilities/helpers";
 import { useList } from "@refinedev/core";
 import { DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY } from "@/graphql/queries";
+import dayjs from "dayjs";
 
 const UpcomingEvents = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const { data, isLoading: eventsLoading } = useList({
+  const { data, isLoading } = useList({
     resource: "events",
     pagination: { pageSize: 5 },
+    sorters: [
+      {
+        field: "startDate",
+        order: "asc",
+      },
+    ],
+    filters: [
+      {
+        field: "startDate",
+        operator: "gte",
+        value: dayjs().format("YYYY-MM-DD"),
+      },
+    ],
     meta: {
       gqlQuery: DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY,
     },
@@ -67,6 +78,19 @@ const UpcomingEvents = () => {
             );
           }}
         ></List>
+      )}
+
+      {!isLoading && data?.data.length === 0 && (
+        <span
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "220px",
+          }}
+        >
+          No upcoming events
+        </span>
       )}
     </Card>
   );
